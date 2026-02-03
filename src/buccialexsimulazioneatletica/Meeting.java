cfb6b3eca79b32e156ee903b4a9bb7d8aff83cfa
gorettileonardo-ui <goretti.leonardo@franchettisalviani.net>
2026-02-03 11:45:08 +0100

    uml terminato
    se spera

---
+++ src/buccialexsimulazioneatletica/Meeting.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package buccialexsimulazioneatletica;
+import java.util.*;
+/**
+ *
+ * @author goretti.leonardo
+ */
+public class Meeting {
+    private ArrayList<Gara> listaGare;
+    
+    public Meeting(){
+        listaGare = new ArrayList<>();
+    }
+    
+    public void creaGara(int durataGara){
+        
+    }
+
+    public ArrayList<Gara> getListaGare() {
+        return listaGare;
+    }
+
+    public void setListaGare(ArrayList<Gara> listaGare) {
+        this.listaGare = listaGare;
+    }
+
+    @Override
+    public String toString() {
+        return "Meeting{" + "listaGare=" + listaGare + '}';
+    }
+    
+    
+        
+}
+
